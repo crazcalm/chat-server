bf4cf3067e4bc4f0e2026aec926884cfa6aa6221
crazcalm <crazcalm@gmail.com>
2015-12-30 08:49:45 -0500

    Nothing works yet, but I am getting closer. I need to check that the log file is still created when server.py is ran in another process

--- features/steps/start_server.py
+++ features/steps/start_server.py
@@ -6,11 +6,17 @@ Note:
 - After creating an instance of unittest.TestCase,
   I can use all of their assert statements!
 """
+def read_server_log():
+    with open("server_log", "r") as f:
+        contexts = f.read()
+    return contexts
 
 @given(u'I want to use port {port}')
 def step_impl(context, port):
-    context.command = shlex.split(
-        "python3 server.py --port {}".format("port"))
+    assert isinstance(int(port), int)
+    #context.command = shlex.split(
+    #    "python3 server.py --port {}".format("port"))
+    context.command = ["python3", "server.py", "--port", str(int(port))]    
 
 @when(u'I start the chat server')
 def step_impl(context):
@@ -18,6 +24,10 @@ def step_impl(context):
 
 @then(u'running the server should have {result} in its output')
 def step_impl(context, result):
-    output = "".join([string for string in context.chat_server.stdout])
-    context.test.assertIn(result, output)
+    answer = {
+        'success': 'serving on',
+        'failure': 'unable to serve on'
+    }           
+    output = read_server_log()
+    context.test.assertIn(answer.get(result, "fucked up"), output)
 
