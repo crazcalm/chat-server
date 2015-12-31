56b2ee32753482405f35a1e8fa0d86edb74c8db6
crazcalm <crazcalm@gmail.com>
2015-12-31 14:52:16 -0500

    started writing the help string docs

---
+++ help_text.py
@@ -0,0 +1,19 @@
+HELP_GENERAL = """
+
+All commands start with a "/".
+
+Command List:
+- /disconnect: Disconnects you from the chat server
+- /whoami: Returns your chat room name
+- /people: Returns the names of the people in the chat room
+- /chatroom: Returns the name of the chat room
+- /help: I am not sure what to write here...
+- /help [command]: Shows the docs for the specified command 
+- /whois [name]: returns the description of that person
+- /msg [name]: sends a direct message to that person
+- /set [property] [value]: Set the value of a specified property
+
+Property List:
+- name
+- description
+"""
