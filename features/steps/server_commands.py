c6cf0955a03e16a3a5e7d56f65672e99b6dd1c96
crazcalm <crazcalm@gmail.com>
2015-12-30 20:44:55 -0500

    I need to start figuring out the logic of the chat server and client relationship

--- features/steps/server_commands.py
+++ features/steps/server_commands.py
@@ -1,22 +1,28 @@
+import socket
+import time
+from subprocess import Popen, PIPE
+
+
 @given(u'I came connected to the chat server as person1')
 def step_impl(context):
-    raise NotImplementedError(
-        u'STEP: Given I came connected to the chat server as person1')
+    context.chat_server = Popen(["python", "server.py"], stdout=PIPE)
+    # Give the server time to start
+    time.sleep(3)
+    context.person1 = socket.create_connection(('localhost', 3333))
 
 @when(u'the client sends {command}')
 def step_impl(context, command):
-    raise NotImplementedError(
-        u'STEP: When the client sends {}'.format(command))
+    context.person1.sendall(command.strip().encode())
 
 @then(u'the client should receive {response}')
 def step_impl(context, response):
-    raise NotImplementedError(
-        u'STEP: Then the client should receive {}'.format(response))
+    output = context.person1.recv(1024).decode()
+    context.test.assertIn(response, output)    
 
 @then(u'the client socket should be disconnected')
 def step_impl(context):
-    raise NotImplementedError(
-        u'STEP: Then the client socket should be disconnected')
+    context.person1.sendall("/disconnect".encode())
+    # need to add an assert to make sure client is disconnected
 
 @given(u'person2 is in the chatroom')
 def step_impl(context):
