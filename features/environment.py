5232c7ea7a5b2fb8ae29dc258c0416e6d8011f19
crazcalm <crazcalm@gmail.com>
2016-01-14 23:30:53 -0500

    Got rid of print lines in the environment file

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,9 @@
 from unittest import TestCase
 
 def before_scenario(context, scenario):
-    print("BEFORE {}".format(scenario))
     context.test = TestCase()
 
 def after_scenario(context, scenario):
-    print("AFTER {}".format(scenario))
     if hasattr(context, "chat_server"):
         if not context.chat_server.poll():
             context.chat_server.kill()
